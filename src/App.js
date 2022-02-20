67399447f5592c8e696c36cccead53eb228196bf
Luiggi Annes Bellincanta <65421097+luiggidev@users.noreply.github.com>
2022-02-20 17:53:59 -0300

    before setting states and methods inside app

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import { Link } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      Start new Reminder
       <Link to="/createReminder">Create Reminder</Link>
-
     </div>
   );
 }
